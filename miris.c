4190aaa40ae29975f28b95881aecaa641f504b1e
KimonSMN <kimonsmirlianos@gmail.com>
2024-10-20 17:42:15 +0300

    contents print to output file

--- miris.c
+++ miris.c
@@ -8,46 +8,56 @@
 int main(int argc, char *argv[])
 {
     Graph graph = NULL;
-
+    FILE *pF1, *pF2;
     int option;
-    while((option = getopt(argc , argv, ":i:o")) != -1){
+    char *iFile = NULL, *oFile = NULL;
+
+    while((option = getopt(argc , argv, ":i:o:")) != -1){
         switch (option)
         {
         case 'i':
-            char accountNameFrom[50];
-            char accountNameTo[50];
-            int amount;
-            char date[11];
-            int line;
-            int ch = 0;
-            FILE *pF = fopen(optarg, "r");
-            if (pF == NULL) {
-                perror("Error opening input file");
-                exit(EXIT_FAILURE);
-            }
-
-            for (ch = getc(pF); ch != EOF; ch = getc(pF))
-                if (ch == '\n')
-                    line = line + 1;
-
-            rewind(pF);
-
-            graph = create_graph(line);
-            // Read transactions from the file and add edges
-            while (fscanf(pF, "%49s %49s %d %10s", accountNameFrom, accountNameTo, &amount, date) == 4) {
-                add_edge(graph, accountNameFrom, accountNameTo, amount, date);
-            }
-
-            fclose(pF);
+            iFile = optarg;
             break;
         case 'o':
-            //σημαία -o είναι το αρχείο εξόδου που την στιγμή της εξόδου της εφαρμογής σας καταγράφει,
-            // την κατάσταση του γράφου όπως αυτή έχει εξελιχτεί με την περάτωση της εφαρμογής.
+            oFile = optarg;
             break;
         case '?':
             exit(-1);
         }
     }
+
+    ////// Read from the input file //////
+
+    if (iFile == NULL) {
+        printf("Input file not provided\n");
+        return -1;
+    }
+
+    char accountNameFrom[50], accountNameTo[50], date[11];
+    int amount, line = 0, ch = 0;
+
+    pF1 = fopen(iFile, "r");
+    if (pF1 == NULL) {
+        printf("error opening file\n");
+        exit(-1);
+    }
+
+    for (ch = getc(pF1); ch != EOF; ch = getc(pF1))
+        if (ch == '\n')
+            line = line + 1;
+
+    rewind(pF1);
+
+    graph = create_graph(line);
+
+    // Read transactions from the file and add edges
+    while (fscanf(pF1, "%49s %49s %d %10s", accountNameFrom, accountNameTo, &amount, date) == 4) {
+        add_edge(graph, accountNameFrom, accountNameTo, amount, date);
+    }
+
+    fclose(pF1);    // Close the file
+    
+    ////// COMMAND LINE //////
 
     bool active = true;
     char command[64];
@@ -69,10 +79,35 @@ int main(int argc, char *argv[])
             printf("Command '%s' not found.\n", command);
         }
     }
+
     print_graph(graph);
-    
+
+    ////// Write to the output file //////
+
+    if (oFile == NULL){
+        printf("Output file not provided\n");
+        return -1;
+    }
+
+    pF2 = fopen(oFile, "w");
+    if (pF2 == NULL) {
+        return -1;
+    }
+
+    struct node *current_node = graph->nodes;
+    while (current_node != NULL) {
+        struct edge *current_edge = current_node->edges;
+        while (current_edge != NULL) {
+            fprintf(pF2, "%s %s %d %s\n", current_node->accountName, current_edge->to_node->accountName, current_edge->amount, current_edge->date);
+            current_edge = current_edge->next;
+        }
+        current_node = current_node->next;
+    }
+
+    fclose(pF2);    // Close the file
+
     destroy_hash_table(graph->htable);
     destroy_graph(graph);
-    
+
     return 0;
 }
